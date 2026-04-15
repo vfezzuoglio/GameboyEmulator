81188dcaffb4b102152d292d5f45fde8317cd971
Vinny <vincent.fezzuoglio@protonmail.com>
2026-04-15 17:57:08 -0400

    initial commit - types.h

---
+++ src/types.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <array>
+#include <string>
+#include <stdexcept>
+
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using s8 = std::int8_t;
+
